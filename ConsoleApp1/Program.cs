89aae4c1e623193b2bf5871457ff18603720c3b8
i3bdolah <xbdullahx@gmail.com>
2024-07-05 23:14:52 +0300

    Implicitly Typed Variables

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,21 +6,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string MyName = "Abdullah";
+            var MyName = "Abdullah";
 
             Console.WriteLine(MyName);
-
-            int x = 10, y = 20;
-            Console.WriteLine("x=" + x);
-            Console.WriteLine("y=" + y);
-
-
-            Console.WriteLine("x+y=" + x + y);
-            Console.WriteLine("x+y=" + (x + y));
-
-            double MyDbl = 24.2343D;
-            char MyLtr = 'a';
-            bool MyBool = false;
         }
     }
 }
