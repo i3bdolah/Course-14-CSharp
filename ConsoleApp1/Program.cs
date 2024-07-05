b822cadfa71eb4f480e19a6499a8fa4bac89449d
i3bdolah <xbdullahx@gmail.com>
2024-07-05 14:03:58 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# I'm Abdullah!");
+            Console.WriteLine("Hello World.");
+            Console.WriteLine("The Sum of 10 + 20 = " + (10+20));
+            Console.WriteLine("My Name is {0}, ans I'm {1} years old.", "Abdullah Al-Shehri", 23);
+        }
+    }
+}
