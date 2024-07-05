e0bf4cbeac4c9ffbc90b015d02593b30ba194e10
i3bdolah <xbdullahx@gmail.com>
2024-07-05 14:08:40 +0300

    Single-line & Multi-line comment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,36 +6,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Useful Escape Characters:\n");
+            // This is Single-line comment.
 
-            //Newline
-            Console.WriteLine("Newline:");
-            Console.WriteLine("Welcome to \nProgrammingAdvices\n");
-
-            //Tab
-            Console.WriteLine("Tab:");
-            Console.WriteLine("Welcome to\tProgrammingAdvices\n");
-
-            //Backspace
-            Console.WriteLine("Backspace:");
-            Console.WriteLine("Welcome to \bProgrammingAdvices\n");
-
-            //Single quote
-            Console.WriteLine("Single Quote:");
-            Console.WriteLine("Welcome to \' ProgrammingAdvices\n");
-
-            //Double quote
-            Console.WriteLine("Double Quote:");
-            Console.WriteLine("Welcome to \" ProgrammingAdvices\n");
-
-            //Backslash
-            Console.WriteLine("Backslash:");
-            Console.WriteLine("Welcome to \\ ProgrammingAdvices\n");
-
-            //Alert
-            Console.WriteLine("Alert:");
-            Console.WriteLine("\a");
-
+            /*
+             
+            This is Multi-line comment.
+             
+             */
         }
     }
 }
