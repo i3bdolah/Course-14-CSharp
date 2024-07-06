85902ab2b513b5adcec3e87a74ed9b924602ae3f
i3bdolah <xbdullahx@gmail.com>
2024-07-06 23:01:04 +0300

    Anonymous Type

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,47 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //  Nullable<int> can be assigned any value
-            //  from -2147483648 to 2147483647, or a null value.
+            //you dont specify any type here , automatically will be specified
+            var student = new { Id = 20, FirstName = "Mohammed", LastName = "Abu-Hadhoud" };
 
-            Nullable<int> i = null;
+            Console.WriteLine("\nExample1:\n");
+            Console.WriteLine(student.Id); //output: 20
+            Console.WriteLine(student.FirstName); //output: Mohammed
+            Console.WriteLine(student.LastName); //output: Abu-Hadhoud
+
+            //You can print like this:
+            Console.WriteLine(student);
+
+
+            //anonymous types are read-only
+            //you cannot change the values of properties as they are read-only.
+
+            // student.Id = 2;//Error: cannot chage value
+            // student.FirstName = "Ali";//Error: cannot chage value
+
+
+            //An anonymous type's property can include another anonymous type.
+            var student2 = new
+            {
+                Id = 20,
+                FirstName = "Mohammed",
+                LastName = "Abu-Hadhoud",
+                Address = new { Id = 1, City = "Amman", Country = "Jordan" }
+            };
+
+            Console.WriteLine("\nExample2:\n");
+            Console.WriteLine(student2.Id);
+            Console.WriteLine(student2.FirstName);
+            Console.WriteLine(student2.LastName);
+
+            Console.WriteLine(student2.Address.Id);
+            Console.WriteLine(student2.Address.City);
+            Console.WriteLine(student2.Address.Country);
+            Console.WriteLine(student2.Address);
+
+            int x = 0;
+            Console.WriteLine(x.GetType());
+            Console.WriteLine(student2.GetType());
         }
     }
 }
