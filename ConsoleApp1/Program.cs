514dce1f38dc2969a5c30d51f79773d16254f1f1
i3bdolah <xbdullahx@gmail.com>
2024-07-06 22:46:40 +0300

    Enums

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,26 +4,48 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        enum enWeekDays
+        {
+            Monday,     // 0
+            Tuesday,    // 1
+            Wednesday,  // 2
+            Thursday,   // 3
+            Friday,     // 4
+            Saturday,   // 5
+            Sunday      // 6
+        }
+
+        //if you set a value , it will auto number everything after it.
+        enum enCategories
+        {
+            Electronics,    // 0
+            Food,           // 1
+            Automotive = 6, // 6
+            Arts,           // 7
+            BeautyCare,     // 8
+            Fashion         // 9
+        }
+
+
+        //Enum can have any numarical data type byte, sbyte, short, ushort, int, uint, long, or ulong
+        //but not string
+
+        enum enCategories2 : byte
+        {
+            Electronics = 1,
+            Food = 5,
+            Automotive = 6,
+            Arts = 10,
+            BeautyCare = 11,
+            Fashion = 15
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Default Values of All Types : \n");
+            enWeekDays Day;
 
+            Day = enWeekDays.Friday;
 
-            Console.WriteLine("Byte = " + default(Byte));
-            Console.WriteLine("sbyte = " + default(sbyte));
-            Console.WriteLine("short = " + default(short));
-            Console.WriteLine("ushort = " + default(ushort));
-            Console.WriteLine("int = " + default(int));
-            Console.WriteLine("uint = " + default(uint));
-            Console.WriteLine("long = " + default(long));
-            Console.WriteLine("ulong = " + default(ulong));
-            Console.WriteLine("float = " + default(float));
-            Console.WriteLine("double = " + default(double));
-            Console.WriteLine("decimal = " + default(decimal) + "\n");
-
-            Console.WriteLine("string = " + default(string));
-            Console.WriteLine("char = " + default(char));
-            Console.WriteLine("bool = " + default(bool));
+            Console.WriteLine(Day);
         }
     }
 }
