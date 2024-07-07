78acef566b17cf394deb8a7491230db420562fbc
i3bdolah <xbdullahx@gmail.com>
2024-07-07 22:35:39 +0300

    Get Current Datetime

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,21 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            DateTime dateTime = new DateTime();
-            Console.WriteLine(dateTime);
+            //assigns default value 01/01/0001 00:00:00
+            DateTime dt1 = new DateTime();
 
-            //assigns year, month, day
-            DateTime dt2 = new DateTime(2023, 12, 31);
-
-            //assigns year, month, day, hour, min, seconds
-            DateTime dt3 = new DateTime(2023, 12, 31, 5, 10, 20);
-
-            //assigns year, month, day, hour, min, seconds, UTC timezone
-            DateTime dt4 = new DateTime(2023, 12, 31, 5, 10, 20, DateTimeKind.Utc);
-
-            Console.WriteLine(dt2);
-            Console.WriteLine(dt3);
-            Console.WriteLine(dt4);
+            dt1 = DateTime.Now;
+            Console.WriteLine(dt1);
         }
     }
 }
