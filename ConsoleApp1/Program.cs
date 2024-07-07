0a5fb1a716abb37b5823422d79da4f7933404826
i3bdolah <xbdullahx@gmail.com>
2024-07-07 22:47:30 +0300

    Subtraction of two dates results in TimeSpan

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,21 +7,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            DateTime dt = new DateTime(2023, 2, 21);
+            DateTime dt1 = new DateTime(2023, 2, 21);
+            DateTime dt2 = new DateTime(2023, 2, 25);
+            TimeSpan result = dt2.Subtract(dt1);
 
-            // Hours, Minutes, Seconds
-            TimeSpan ts = new TimeSpan(49, 25, 34);
-            Console.WriteLine(ts.GetType());
-            Console.WriteLine(ts);
-            Console.WriteLine(ts.Days);
-            Console.WriteLine(ts.Hours);
-            Console.WriteLine(ts.Minutes);
-            Console.WriteLine(ts.Seconds);
-
-            //this will add time span to the date.
-            DateTime newDate = dt.Add(ts);
-
-            Console.WriteLine(newDate);
+            Console.WriteLine(result.ToString());
         }
     }
 }
