f969e313a5a288b710e49ef377baf77c0c37c4db
i3bdolah <xbdullahx@gmail.com>
2024-07-07 23:24:20 +0300

    String Interpolation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,34 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string S1 = "Abdullah Al-Shehri";
+            //  String Interpolation
 
-            Console.WriteLine(S1.Length);
+            string firstName = "Abdullah";
+            string lastName = "Al-Shehri";
+            string code = "107";
 
-            //this will take 5 characters staring position 2
-            Console.WriteLine(S1.Substring(2, 5));
-            Console.WriteLine(S1.ToLower());
-            Console.WriteLine(S1.ToUpper());
-            Console.WriteLine(S1[2]);
-            Console.WriteLine(S1.Insert(3, "KKKK"));
-            Console.WriteLine(S1.Replace("h", "*"));
-            Console.WriteLine(S1.IndexOf("h"));
-            Console.WriteLine(S1.Contains("h"));
-            Console.WriteLine(S1.Contains("x"));
-            Console.WriteLine(S1.LastIndexOf("h"));
+            //You shold use $ to $ to identify an interpolated string 
+            string fullName = $"Mr. {firstName} {lastName}, Code: {code}";
 
-            string S2 = "Ali,Ahmed,Khalid";
-
-            string[] NamesList = S2.Split(',');
-
-            Console.WriteLine(NamesList[0]);
-            Console.WriteLine(NamesList[1]);
-            Console.WriteLine(NamesList[2]);
-
-            string S3 = "  Al-Shehri  ";
-            Console.WriteLine(S3.Trim());
-            Console.WriteLine(S3.TrimStart());
-            Console.WriteLine(S3.TrimEnd());
+            Console.WriteLine(fullName);
         }
     }
 }
