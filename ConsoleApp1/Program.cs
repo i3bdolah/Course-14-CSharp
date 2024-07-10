90f85473a5971572184cd84689dc976097f94503
i3bdolah <xbdullahx@gmail.com>
2024-07-10 22:47:50 +0300

    Count , Sum And Average Using System.Linq

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,13 +8,18 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // Note that we used System.Linq above.
-            int[] numbers = { 51, -1, 2, 14, 18, 40, 178 };
+            int[] numbers = { 20, 22, 19, 18, 1 };
 
-            // get the minimum element
-            Console.WriteLine("Smallest  Element: " + numbers.Min());
 
-            // Max() returns the largest number in array
-            Console.WriteLine("Largest Element: " + numbers.Max());
+
+            // compute Count
+            Console.WriteLine("Count : " + numbers.Count());
+
+            // compute Sum
+            Console.WriteLine("Sum : " + numbers.Sum());
+
+            // compute the average
+            Console.WriteLine("Average: " + numbers.Average());
         }
     }
 }
