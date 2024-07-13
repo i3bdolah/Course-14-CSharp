98e0340b28ca5fdba33766b42023eca4ddaa5c33
ABDULLAH BAN (Mac) <i3bdolah@outlook.com>
2024-07-13 12:49:09 +0300

    Default Parameter Value

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,14 +6,14 @@ namespace ConsoleApp1
     internal class Program
     {
 
-        static void MyFirstMethod(string name, byte age)
+        static void MyFirstMethod(string name, byte age = 23)
         {
             Console.WriteLine($"{name} writes his first method at the age of {age}.");
         }
 
         static void Main(string[] args)
         {
-            MyFirstMethod("Abdullah", 23);
+            MyFirstMethod("Abdullah");
         }
     }
 }
