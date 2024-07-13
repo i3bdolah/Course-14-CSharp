2fc10d3802942732c809d1b1c2af8e666e0f5af6
ABDULLAH BAN (Mac) <i3bdolah@outlook.com>
2024-07-13 12:46:13 +0300

    Methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,14 +5,15 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+
+        static void MyFirstMethod()
+        {
+            Console.WriteLine("Abdullah writes hes first method!");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Max of 5, 10 is: {0}", Math.Max(5, 10));
-            Console.WriteLine("Min of 5, 10 is: {0}", Math.Min(5, 10));
-            Console.WriteLine("Squir Root of 64 is: {0}", Math.Sqrt(64));
-            Console.WriteLine("Absolute (positive) value of  -4.7 is: {0}", Math.Abs(-4.7));
-            Console.WriteLine("Round of 9.99 is: {0}", Math.Round(9.99));
-
+            MyFirstMethod();
         }
     }
 }
