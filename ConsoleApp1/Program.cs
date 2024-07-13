539f9a8e5291f1320327433cade2095dbe941c18
ABDULLAH BAN (Mac) <i3bdolah@outlook.com>
2024-07-13 12:53:27 +0300

    Method Overloading

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,15 +5,27 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static void MyMethod(string child1, string child2, string child3)
+        static int Sum(int Num1, int Num2)
         {
-            Console.WriteLine("The youngest child is: " + child3);
+            return Num1 + Num2;
+        }
+
+        static int Sum(int Num1, int Num2, int Num3)
+        {
+            return Num1 + Num2 + Num3;
+        }
+        static int Sum(int Num1, int Num2, int Num3, int Num4)
+        {
+            return Num1 + Num2 + Num3 + Num4;
         }
 
         static void Main(string[] args)
         {
-            //see the order of sending parameters is not important.
-            MyMethod(child3: "Omar", child1: "Saqer", child2: "Hamza");
+            //we have 3 diffrent methods but with the same name.
+            Console.WriteLine(Sum(10, 20));
+            Console.WriteLine(Sum(10, 20, 30));
+            Console.WriteLine(Sum(10, 20, 30, 40));
+            Console.ReadKey();
         }
     }
 }
