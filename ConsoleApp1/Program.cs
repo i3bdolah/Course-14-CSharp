6be72dc8140ac7b4c69fee8e01592d7ff130bc32
ABDULLAH BAN (Mac) <i3bdolah@outlook.com>
2024-07-13 14:03:18 +0300

    Exception

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,27 +5,17 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static int Sum(int Num1, int Num2)
-        {
-            return Num1 + Num2;
-        }
-
-        static int Sum(int Num1, int Num2, int Num3)
-        {
-            return Num1 + Num2 + Num3;
-        }
-        static int Sum(int Num1, int Num2, int Num3, int Num4)
-        {
-            return Num1 + Num2 + Num3 + Num4;
-        }
-
         static void Main(string[] args)
         {
-            //we have 3 diffrent methods but with the same name.
-            Console.WriteLine(Sum(10, 20));
-            Console.WriteLine(Sum(10, 20, 30));
-            Console.WriteLine(Sum(10, 20, 30, 40));
-            Console.ReadKey();
+            try
+            {
+                int[] Arr = { 1, 2, 3, 4, 5 };
+                Console.WriteLine(Arr[10]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
